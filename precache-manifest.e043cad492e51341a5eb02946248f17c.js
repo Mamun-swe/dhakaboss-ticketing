b5003e2d0aaff75a8e54acfba49fb1f476b5da60
Mamun-swe <mhmamun166009@gmail.com>
2020-05-30 00:44:07 +0600

    Updates

---
+++ precache-manifest.e043cad492e51341a5eb02946248f17c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7278fad0f43b84909047b4a8aa0eb52d",
+    "url": "/dhakaboss-ticketing/index.html"
+  },
+  {
+    "revision": "d0e6d2bf4b950a589627",
+    "url": "/dhakaboss-ticketing/static/css/2.a5d7ed5b.chunk.css"
+  },
+  {
+    "revision": "6e2a335bc178fe69e041",
+    "url": "/dhakaboss-ticketing/static/css/main.dfa8d7db.chunk.css"
+  },
+  {
+    "revision": "d0e6d2bf4b950a589627",
+    "url": "/dhakaboss-ticketing/static/js/2.27182269.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/dhakaboss-ticketing/static/js/2.27182269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2a335bc178fe69e041",
+    "url": "/dhakaboss-ticketing/static/js/main.acc2c598.chunk.js"
+  },
+  {
+    "revision": "9e2446f8d3fc34d27f66",
+    "url": "/dhakaboss-ticketing/static/js/runtime-main.9c472b21.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/dhakaboss-ticketing/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/dhakaboss-ticketing/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/dhakaboss-ticketing/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "531e07726b523996335dd5d03c9c959a",
+    "url": "/dhakaboss-ticketing/static/media/banner.531e0772.jpg"
+  },
+  {
+    "revision": "9907c81e12a92d0f1a4c2525ee16caa8",
+    "url": "/dhakaboss-ticketing/static/media/biman.9907c81e.png"
+  },
+  {
+    "revision": "175c1b9e9c4c9699a00417861f5db2a6",
+    "url": "/dhakaboss-ticketing/static/media/eiffel.175c1b9e.png"
+  },
+  {
+    "revision": "22936743ee073d3e7b0edee14a6a88ae",
+    "url": "/dhakaboss-ticketing/static/media/logo.22936743.png"
+  },
+  {
+    "revision": "d7ddac68f28005851379ded0fbe7de64",
+    "url": "/dhakaboss-ticketing/static/media/paypal.d7ddac68.png"
+  },
+  {
+    "revision": "7a0efa458949b0e5846113565121fbe2",
+    "url": "/dhakaboss-ticketing/static/media/wallet.7a0efa45.jpg"
+  }
+]);
